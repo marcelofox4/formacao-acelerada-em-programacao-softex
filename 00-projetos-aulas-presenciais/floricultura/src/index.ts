29cbf66a24468d0330c71fbcb3bb00201115952b
marcelofox4 <marcelofox4@hotmail.com>
2023-10-04 07:14:01 -0300

    Criação dos métodos http para as rotas de cliente

--- 00-projetos-aulas-presenciais/floricultura/src/index.ts
+++ 00-projetos-aulas-presenciais/floricultura/src/index.ts
@@ -18,8 +18,43 @@ server.use(express.json());
 
 const port = 3333;
 
+// http://localhost:3333/clients
 server.get('/clients', (req, res) => {
     res.send(database.showClients());
+})
+
+// http://localhost:3333/client/888.777.888-77/João/81 96655-4422/Rua E, 05
+server.post('/client/:cpf/:name/:phoneNumber/:address', (req, res) => {
+    //TODO: validar os parametros.
+    let clientCpf = req.params.cpf;
+    let clientName = req.params.name;
+    let clientPhoneNumber = req.params.phoneNumber;
+    let clientAddress = req.params.address;
+
+    let client = new Client(clientCpf, clientName, clientPhoneNumber, clientAddress);
+    database.addClient(client);
+
+    res.send(`Cliente ${clientName} cadastrado com Sucesso!`);
+})
+
+// http://localhost:3333/client/5/Rua das larangeiras, 400/ 81 91111-1111
+server.put('/client/:id/:Address/:phoneNumber', (req, res) => {
+    let id = Number(req.params.id);
+    let address = req.params.Address;
+    let phoneNumber = req.params.phoneNumber;
+    let client = database.searchClient(id);
+
+    client.setAddress(address);
+    client.setPhoneNumber(phoneNumber);
+    res.send("Atualização realizada com sucesso!")
+})
+
+// http://localhost:3333/client/3
+server.delete('/client/:id', (req, res) => {
+    let id = Number(req.params.id);
+    database.deleteClient(id);
+
+    res.send(`Cliente com ID: ${id} foi removido com sucesso!`)
 })
 
 server.listen(port, () => {
