b6d6bf0dba39a6abd3688af465bd3cba77dfb110
marcelofox4 <marcelofox4@hotmail.com>
2023-11-07 23:50:57 -0300

    implementação da conexão com o banco de dados

---
+++ 00-projetos-aulas-presenciais/conectando-um-projeto-nodejs-com-o-banco-de-dados-utilizando-o-typeorm/src/database/data-source.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "teste_typeorm",
+    synchronize: true,
+    logging: false,
+    entities: [],
+})
--- 00-projetos-aulas-presenciais/conectando-um-projeto-nodejs-com-o-banco-de-dados-utilizando-o-typeorm/src/server.ts
+++ 00-projetos-aulas-presenciais/conectando-um-projeto-nodejs-com-o-banco-de-dados-utilizando-o-typeorm/src/server.ts
@@ -1,7 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+import { AppDataSource } from "./database/data-source";
 
 const app = express();
 
 
-
-app.listen(3333, () => {console.log("Servidor funcionado!")});
+AppDataSource.initialize().then(async () => {
+    console.log("Banco de dados Conectado!");
+    app.listen(3333, () => console.log("Servidor Funcionado"));
+})
